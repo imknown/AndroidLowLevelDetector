4d2528cda4276c68b2ca039cd45f82fa245ec520
imknown <imknown@qq.com>
2020-03-14 12:47:07 +0800

    Fix library loading sequence again...

--- app/src/main/java/net/imknown/android/forefrontinfo/ui/others/OthersViewModel.kt
+++ app/src/main/java/net/imknown/android/forefrontinfo/ui/others/OthersViewModel.kt
@@ -28,12 +28,6 @@ class OthersViewModel : BaseListViewModel() {
         private const val BINDER64_PROTOCOL_VERSION = 8
     }
 
-    init {
-        viewModelScope.launch(Dispatchers.IO) {
-            System.loadLibrary("BinderDetector")
-        }
-    }
-
     private val _rawProp by lazy {
         MyApplication.sharedPreferences.booleanEventLiveData(
             viewModelScope,
@@ -167,6 +161,8 @@ class OthersViewModel : BaseListViewModel() {
         driver: String,
         @StringRes titleId: Int
     ) {
+        System.loadLibrary("BinderDetector")
+
         val binderVersion = getBinderVersion(driver)
 
         @StringRes val binderStatusId = if (binderVersion == -ERRNO_NO_SUCH_FILE_OR_DIRECTORY) {
