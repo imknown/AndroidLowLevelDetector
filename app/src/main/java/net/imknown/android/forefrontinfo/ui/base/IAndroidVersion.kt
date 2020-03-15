c494e0c6d3ccce328e13b2faade2d1fa2e4409c7
imknown <imknown@qq.com>
2020-03-15 22:15:21 +0800

    Revert some IAndroidVersion logic

--- app/src/main/java/net/imknown/android/forefrontinfo/ui/base/IAndroidVersion.kt
+++ app/src/main/java/net/imknown/android/forefrontinfo/ui/base/IAndroidVersion.kt
@@ -7,44 +7,23 @@ import net.imknown.android.forefrontinfo.ui.home.model.Lld
 interface IAndroidVersion {
     companion object {
         private const val CODENAME_RELEASE = "REL"
-
-        private const val Build_VERSION_CODES_R = 30
     }
 
-    private fun isAtLeastAndroid(version: Int, previewVersion: Int) =
-        (Build.VERSION.SDK_INT >= version && Build.VERSION.CODENAME == CODENAME_RELEASE)
-                || (Build.VERSION.SDK_INT >= previewVersion && Build.VERSION.CODENAME != CODENAME_RELEASE)
+    fun isAtLeastAndroid6() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+    fun isAtLeastAndroid7() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+    fun isAtLeastAndroid8() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+    fun isAtLeastAndroid9() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+    fun isAtLeastAndroid10() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
 
-    fun isAndroid(version: Int, previewVersion: Int) =
-        (Build.VERSION.SDK_INT == version && Build.VERSION.CODENAME == CODENAME_RELEASE)
-                || (Build.VERSION.SDK_INT == previewVersion && Build.VERSION.CODENAME != CODENAME_RELEASE)
-
-    fun isAtLeastAndroid6() =
-        isAtLeastAndroid(Build.VERSION_CODES.M, Build.VERSION_CODES.LOLLIPOP_MR1)
-
-    fun isAtLeastAndroid7() =
-        isAtLeastAndroid(Build.VERSION_CODES.N, Build.VERSION_CODES.M)
-
-    fun isAtLeastAndroid8() =
-        isAtLeastAndroid(Build.VERSION_CODES.O, Build.VERSION_CODES.N_MR1)
-
-    fun isAtLeastAndroid9() =
-        isAtLeastAndroid(Build.VERSION_CODES.P, Build.VERSION_CODES.O_MR1)
-
-    fun isAndroid10() = isAndroid(Build.VERSION_CODES.Q, Build.VERSION_CODES.P)
-
-    fun isAtLeastAndroid10() =
-        isAtLeastAndroid(Build.VERSION_CODES.Q, Build.VERSION_CODES.P)
-
-    fun isAtLeastAndroid11() =
-        isAtLeastAndroid(Build_VERSION_CODES_R, Build.VERSION_CODES.Q)
+    fun isStableAndroid10() =
+        Build.VERSION.SDK_INT == Build.VERSION_CODES.Q && Build.VERSION.CODENAME == CODENAME_RELEASE
 
     @SuppressLint("ObsoleteSdkInt")
     fun isLatestStableAndroid(lld: Lld) =
-        Build.VERSION.SDK_INT >= lld.android.stable.api.toInt() && Build.VERSION.CODENAME == CODENAME_RELEASE
+        Build.VERSION.SDK_INT >= lld.android.stable.api.toInt()
 
     fun isLatestPreviewAndroid(lld: Lld) =
-        Build.VERSION.RELEASE >= lld.android.preview.name && Build.VERSION.CODENAME != CODENAME_RELEASE
+        Build.VERSION.RELEASE >= lld.android.preview.name
 
     @SuppressLint("ObsoleteSdkInt")
     fun isSupportedByUpstreamAndroid(lld: Lld) =
@@ -53,6 +32,6 @@ interface IAndroidVersion {
     // https://github.com/square/leakcanary/issues/1594
     // https://issuetracker.google.com/issues/139738913
     // https://github.com/pytorch/cpuinfo/blob/master/test/build.prop/huawei-p9-lite.log
-    fun is10Leak() = isAndroid10()
+    fun is10Leak() = isStableAndroid10()
             && (Build.ID.split('.').size < 2 || Build.ID.split('.')[1] < "191205")
 }
