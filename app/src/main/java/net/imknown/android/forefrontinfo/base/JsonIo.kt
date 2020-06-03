b35aac4bb6cab79ce2f5f18dbc2790d69adfe856
imknown <imknown@qq.com>
2020-06-03 20:30:06 +0800

    Simply singletons

--- app/src/main/java/net/imknown/android/forefrontinfo/base/JsonIo.kt
+++ app/src/main/java/net/imknown/android/forefrontinfo/base/JsonIo.kt
@@ -7,66 +7,64 @@ import net.imknown.android.forefrontinfo.ui.home.model.Lld
 import java.io.BufferedReader
 import java.io.File
 
-class JsonIo {
-    companion object {
-        const val LLD_JSON_NAME = "lld.json"
+object JsonIo {
+    const val LLD_JSON_NAME = "lld.json"
 
-        val savedLldJsonFile by lazy {
-            File(MyApplication.getDownloadDir(), LLD_JSON_NAME)
-        }
-
-        lateinit var lld: Lld
-            private set
-
-        fun prepareLld() {
-            lld = savedLldJsonFile.fromJson()
-        }
-
-        /**
-         * https://www.programiz.com/kotlin-programming/examples/string-date
-         */
-        fun whetherNeedCopyAssets(assets: AssetManager): Boolean {
-            if (savedLldJsonFile.exists()) {
-                prepareLld()
-
-                val savedLldVersion = lld.version
-                val assetLldVersion = getAssetLldVersion(assets)
-
-                if (savedLldVersion >= assetLldVersion) {
-                    return false
-                }
-            }
-
-            return true
-        }
-
-        /**
-         * https://discuss.kotlinlang.org/t/copy-file-from-res/7068/10
-         */
-        fun copyJsonFromAssetsToContextFilesDir(
-            assets: AssetManager,
-            savedFile: File,
-            assetName: String
-        ) {
-            assets.open(assetName).use { inStream ->
-                savedFile.parentFile?.mkdirs()
-
-                savedFile.outputStream().use { outStream ->
-                    outStream.let {
-                        inStream.copyTo(it)
-
-                        prepareLld()
-                    }
-                }
-            }
-        }
-
-        fun getAssetLldVersion(assets: AssetManager) =
-            assets.open(LLD_JSON_NAME)
-                .bufferedReader()
-                .use(BufferedReader::readText)
-                .fromJson<Lld>().version
+    val savedLldJsonFile by lazy {
+        File(MyApplication.getDownloadDir(), LLD_JSON_NAME)
     }
+
+    lateinit var lld: Lld
+        private set
+
+    fun prepareLld() {
+        lld = savedLldJsonFile.fromJson()
+    }
+
+    /**
+     * https://www.programiz.com/kotlin-programming/examples/string-date
+     */
+    fun whetherNeedCopyAssets(assets: AssetManager): Boolean {
+        if (savedLldJsonFile.exists()) {
+            prepareLld()
+
+            val savedLldVersion = lld.version
+            val assetLldVersion = getAssetLldVersion(assets)
+
+            if (savedLldVersion >= assetLldVersion) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    /**
+     * https://discuss.kotlinlang.org/t/copy-file-from-res/7068/10
+     */
+    fun copyJsonFromAssetsToContextFilesDir(
+        assets: AssetManager,
+        savedFile: File,
+        assetName: String
+    ) {
+        assets.open(assetName).use { inStream ->
+            savedFile.parentFile?.mkdirs()
+
+            savedFile.outputStream().use { outStream ->
+                outStream.let {
+                    inStream.copyTo(it)
+
+                    prepareLld()
+                }
+            }
+        }
+    }
+
+    fun getAssetLldVersion(assets: AssetManager) =
+        assets.open(LLD_JSON_NAME)
+            .bufferedReader()
+            .use(BufferedReader::readText)
+            .fromJson<Lld>().version
 }
 
 inline fun <reified T : Any> File.fromJson(): T =
