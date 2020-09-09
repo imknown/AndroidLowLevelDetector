9993143afcff803f671a307e8933a42006969f9b
imknown <imknown@qq.com>
2020-09-09 22:57:43 +0800

    Update lld.sjon

--- app/src/main/java/net/imknown/android/forefrontinfo/ui/home/model/Lld.kt
+++ app/src/main/java/net/imknown/android/forefrontinfo/ui/home/model/Lld.kt
@@ -47,7 +47,7 @@ data class Lld(
         ) : BaseInfo()
 
         // https://android.googlesource.com/platform/build/+refs
-        // https://android.googlesource.com/platform/build/+/refs/tags/android-10.0.0_r45/core/build_id.mk
+        // https://android.googlesource.com/platform/build/+/refs/tags/android-11.0.0_r3/core/build_id.mk
         // https://android.googlesource.com/platform/build/+/master/core/build_id.mk
         //
         // https://developers.google.com/android/images
@@ -90,9 +90,9 @@ data class Lld(
     // https://android.googlesource.com/platform/system/core/+/master/shell_and_utilities/
     @Keep
     data class Toyboxes(
-        // https://android.googlesource.com/platform/external/toybox/+/refs/tags/android-10.0.0_r45/main.c
+        // https://android.googlesource.com/platform/external/toybox/+/refs/tags/android-11.0.0_r3/main.c
         val stable: BaseInfo,
-        // https://android.googlesource.com/platform/external/toybox/+/refs/tags/android-8.0.0_r49/main.c
+        // https://android.googlesource.com/platform/external/toybox/+/refs/tags/android-8.0.0_r50/main.c
         val support: BaseInfo,
         // https://android.googlesource.com/platform/external/toybox/+/android10-mainline-resolv-release/main.c
         val mainline: BaseInfo,
