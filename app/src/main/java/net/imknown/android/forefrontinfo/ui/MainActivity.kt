3b6764d93c14e8d981f27912729713dcc362cd5d
imknown <imknown@qq.com>
2021-10-14 20:33:29 +0800

    Simplify Fragment transaction logic

--- app/src/main/java/net/imknown/android/forefrontinfo/ui/MainActivity.kt
+++ app/src/main/java/net/imknown/android/forefrontinfo/ui/MainActivity.kt
@@ -3,15 +3,25 @@ package net.imknown.android.forefrontinfo.ui
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commitNow
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import net.imknown.android.forefrontinfo.R
 import net.imknown.android.forefrontinfo.base.MyApplication
 import net.imknown.android.forefrontinfo.base.mvvm.IView
 import net.imknown.android.forefrontinfo.base.mvvm.viewBinding
 import net.imknown.android.forefrontinfo.databinding.MainActivityBinding
+import net.imknown.android.forefrontinfo.ui.home.HomeFragment
+import net.imknown.android.forefrontinfo.ui.others.OthersFragment
+import net.imknown.android.forefrontinfo.ui.prop.PropFragment
+import net.imknown.android.forefrontinfo.ui.settings.SettingsFragment
 
 class MainActivity : AppCompatActivity(), IView {
 
@@ -28,14 +38,16 @@ class MainActivity : AppCompatActivity(), IView {
 
         initViews()
 
-        mainViewModel.showTheTopFragmentOfTheStack(savedInstanceState, supportFragmentManager)
+        if (savedInstanceState == null) {
+            supportFragmentManager.switch(R.id.navigation_home, true)
+        }
     }
 
     private fun initViews() {
         initSubtitle()
 
         binding.bottomNavigationView.setOnItemSelectedListener {
-            mainViewModel.switchFragment(supportFragmentManager, it.itemId)
+            supportFragmentManager.switch(it.itemId)
 
             true
         }
@@ -51,5 +63,38 @@ class MainActivity : AppCompatActivity(), IView {
                 .get(binding.toolbar) as TextView
             subtitleTextView.isVerticalScrollBarEnabled = false
         }
+    }
+
+    private fun FragmentManager.switch(@IdRes selectedId: Int, isFirst: Boolean = false) {
+        val lastId = mainViewModel.lastId
+        if (selectedId == lastId && !isFirst) {
+            return
+        }
+
+        commitNow(true) {
+            setCustomAnimations(R.anim.drop_scale, FragmentTransaction.TRANSIT_NONE)
+
+            val selectedFragment = findFragmentByTag(selectedId.toString())
+                ?: createFragment(selectedId)
+            show(selectedFragment)
+
+            findFragmentByTag(lastId.toString())?.let {
+                hide(it)
+            }
+
+            setReorderingAllowed(true)
+        }
+
+        mainViewModel.setSavedStateLastId(selectedId)
+    }
+
+    private fun FragmentTransaction.createFragment(@IdRes id: Int): Fragment = when (id) {
+        R.id.navigation_home -> HomeFragment.newInstance()
+        R.id.navigation_others -> OthersFragment.newInstance()
+        R.id.navigation_prop -> PropFragment.newInstance()
+        R.id.navigation_settings -> SettingsFragment.newInstance()
+        else -> throw Exception()
+    }.apply {
+        add(R.id.container, this, id.toString())
     }
 }
--- app/src/main/java/net/imknown/android/forefrontinfo/ui/MainViewModel.kt
+++ app/src/main/java/net/imknown/android/forefrontinfo/ui/MainViewModel.kt
@@ -1,17 +1,9 @@
 package net.imknown.android.forefrontinfo.ui
 
-import android.os.Bundle
 import androidx.annotation.IdRes
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import net.imknown.android.forefrontinfo.R
-import net.imknown.android.forefrontinfo.ui.home.HomeFragment
-import net.imknown.android.forefrontinfo.ui.others.OthersFragment
-import net.imknown.android.forefrontinfo.ui.prop.PropFragment
-import net.imknown.android.forefrontinfo.ui.settings.SettingsFragment
 
 class MainViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
@@ -20,109 +12,14 @@ class MainViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel(
     }
 
     @IdRes
-    private var lastId = getSavedStateLastId()
+    var lastId = getSavedStateLastId()
 
-    fun switchFragment(
-        supportFragmentManager: FragmentManager,
-        @IdRes selectedId: Int
-    ) {
-        if (selectedId == lastId) {
-            return
-        }
-
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-            .setCustomAnimations(R.anim.drop_scale, FragmentTransaction.TRANSIT_NONE)
-
-        val isFragmentCreated = isFragmentCreated(supportFragmentManager, selectedId)
-        val selectedFragment = getFragment(supportFragmentManager, fragmentTransaction, selectedId)
-
-        if (isFragmentCreated) {
-            fragmentTransaction.show(selectedFragment)
-        }
-
-        val lastFragment = getFragment(supportFragmentManager, fragmentTransaction, lastId)
-        fragmentTransaction.hide(lastFragment)
-
-        fragmentTransaction.setReorderingAllowed(true)
-
-        fragmentTransaction.commitNowAllowingStateLoss()
-
-        lastId = selectedId
-
-        setSavedStateLastId()
+    fun setSavedStateLastId(@IdRes id: Int) {
+        lastId = id
+        savedStateHandle[SAVED_STATE_HANDLE_KEY_LAST_ID] = id
     }
 
-    private fun findFragmentByTag(supportFragmentManager: FragmentManager, @IdRes id: Int) =
-        supportFragmentManager.findFragmentByTag(id.toString())
-
-    private fun isFragmentCreated(supportFragmentManager: FragmentManager, @IdRes id: Int) =
-        findFragmentByTag(supportFragmentManager, id) != null
-
-    private fun getFragment(
-        supportFragmentManager: FragmentManager,
-        fragmentTransaction: FragmentTransaction,
-        @IdRes id: Int
-    ) = findFragmentByTag(supportFragmentManager, id)
-        ?: createFragment(fragmentTransaction, id)
-
-    /**
-     * 'top' means the top fragment in the stack, not the first tab in the order of BottomNavigationView
-     * This is because of the recreation of Activity
-     */
-    fun showTheTopFragmentOfTheStack(
-        savedInstanceState: Bundle?,
-        supportFragmentManager: FragmentManager
-    ) {
-        if (savedInstanceState != null) {
-            // This is the activity which is recreated
-            return
-        }
-
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-            .setCustomAnimations(R.anim.drop_scale, FragmentTransaction.TRANSIT_NONE)
-
-        createFragment(fragmentTransaction, lastId)
-
-        fragmentTransaction.setReorderingAllowed(true)
-
-        fragmentTransaction.commitNowAllowingStateLoss()
-    }
-
-    private fun createFragment(
-        fragmentTransaction: FragmentTransaction,
-        @IdRes id: Int
-    ): Fragment {
-        val fragment: Fragment = when (id) {
-            R.id.navigation_home -> {
-                HomeFragment.newInstance()
-            }
-
-            R.id.navigation_others -> {
-                OthersFragment.newInstance()
-            }
-
-            R.id.navigation_prop -> {
-                PropFragment.newInstance()
-            }
-
-            R.id.navigation_settings -> {
-                SettingsFragment.newInstance()
-            }
-
-            else -> {
-                throw Exception()
-            }
-        }
-
-        fragmentTransaction.add(R.id.container, fragment, id.toString())
-
-        return fragment
-    }
-
-    private fun setSavedStateLastId() {
-        savedStateHandle[SAVED_STATE_HANDLE_KEY_LAST_ID] = lastId
-    }
-
+    @IdRes
     private fun getSavedStateLastId() = savedStateHandle[SAVED_STATE_HANDLE_KEY_LAST_ID]
         ?: R.id.navigation_home
 }
