4862066a0090406411aceba251eeb6cc388e1638
imknown <imknown@qq.com>
2021-12-15 20:54:24 +0800

    Format codes

--- app/src/main/java/net/imknown/android/forefrontinfo/ui/base/list/BasePureListViewModel.kt
+++ app/src/main/java/net/imknown/android/forefrontinfo/ui/base/list/BasePureListViewModel.kt
@@ -2,7 +2,6 @@ package net.imknown.android.forefrontinfo.ui.base.list
 
 import net.imknown.android.forefrontinfo.R
 import net.imknown.android.forefrontinfo.base.MyApplication
-import java.util.*
 
 abstract class BasePureListViewModel : BaseListViewModel() {
     protected fun add(tempModels: ArrayList<MyModel>, title: String, detail: String?) {
