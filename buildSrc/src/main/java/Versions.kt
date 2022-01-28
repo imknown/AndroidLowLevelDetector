bb198eb19ec78c1461760b001e743a76ae9d3597
imknown <imknown@qq.com>
2022-01-28 20:11:30 +0800

    Update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
     object AndroidBuild {
-        const val androidGradlePlugin = "7.1.0"
+        const val androidGradlePlugin = "7.2.0-beta01"
 
         // https://developer.android.com/studio/releases/build-tools.html#notes
-        const val buildToolsVersion = "32.0.0"
+        const val buildToolsVersion = "32.1.0-rc1"
 
         const val minSdkVersion = 21
         const val compileSdkVersion = 32
