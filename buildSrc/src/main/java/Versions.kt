51c41a2a70e93e3f0df807938f9bc2fb913d8c67
imknown <imknown@qq.com>
2022-04-03 12:20:41 +0800

    Begin version 1.15.9(61)

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -14,8 +14,8 @@ object Versions {
         const val ndk = "25.0.8221429 rc2"
         const val cmake = "3.23.0-rc3"
 
-        const val versionCode = 60
-        const val versionName = "1.15.8"//_unpublished"
+        const val versionCode = 61
+        const val versionName = "1.15.9_unpublished"
     }
 
     // https://developer.android.com/studio/write/java8-support#library-desugaring
