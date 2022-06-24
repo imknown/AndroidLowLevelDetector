f7a796d7107843aa56a56c9f22fe95614e970cee
imknown <imknown@qq.com>
2022-06-24 19:23:23 +0800

    Improve Datetime APIs

--- base/src/main/java/net/imknown/android/forefrontinfo/base/extension/DateTimeExt.kt
+++ base/src/main/java/net/imknown/android/forefrontinfo/base/extension/DateTimeExt.kt
@@ -6,23 +6,21 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 fun String.formatToLocalZonedDatetimeString(): String {
-    val patternZonedDatetime = "yyyy-MM-dd HH:mm Z"
-    val formatter = DateTimeFormatter.ofPattern(patternZonedDatetime)
-    val zonedDateTime =
-        // ZonedDateTime.ofInstant(ZonedDateTime.parse(this, formatter).toInstant(), ZoneId.systemDefault())
-        ZonedDateTime.parse(this, formatter).toInstant().atZone(ZoneId.systemDefault())
-    return formatter.format(zonedDateTime)
+    val pattern = "yyyy-MM-dd HH:mm Z"
+    val formatter = DateTimeFormatter.ofPattern(pattern)
+    val instant = ZonedDateTime.parse(this, formatter).toInstant()
+    val datetime = instant.atZone(ZoneId.systemDefault())
+    return formatter.format(datetime)
 }
 
 fun Long.formatToLocalZonedDatetimeString(): String {
-    val patternDatetime = "yyyy-MM-dd HH:mm:ss Z"
-    val formatter = DateTimeFormatter.ofPattern(patternDatetime)
-    val localDateTime =
-        // Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault()).toLocalDateTime()
-        ZonedDateTime.ofInstant(Instant.ofEpochMilli(this), ZoneId.systemDefault())
-
-    return formatter.format(localDateTime)
+    val pattern = "yyyy-MM-dd HH:mm:ss Z"
+    val formatter = DateTimeFormatter.ofPattern(pattern)
+    val instant = Instant.ofEpochMilli(this)
+    val datetime = instant.atZone(ZoneId.systemDefault())
+    return formatter.format(datetime)
 }
 
-fun isChinaMainlandTimezone() =
-    ZoneId.systemDefault().id == "Asia/Shanghai" || ZoneId.systemDefault().id == "Asia/Urumqi"
+fun isChinaMainlandTimezone() = with(ZoneId.systemDefault()) {
+    id == "Asia/Shanghai" || id == "Asia/Urumqi"
+}
--- buildSrc/src/main/java/Ext.kt
+++ buildSrc/src/main/java/Ext.kt
@@ -3,9 +3,12 @@ import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
-fun getCurrentDatetime(): String? {
-    val now = Instant.now().atZone(ZoneId.systemDefault())
-    return DateTimeFormatter.ofPattern("yyyyMMdd-HHmm").format(now)
+fun getCurrentDatetime(): String {
+    val pattern = "yyyyMMdd-HHmm"
+    val formatter = DateTimeFormatter.ofPattern(pattern)
+    val instant = Instant.now()
+    val datetime = instant.atZone(ZoneId.systemDefault())
+    return formatter.format(datetime)
 }
 
 fun String.execute() = ProcessGroovyMethods.getText(ProcessGroovyMethods.execute(this)).trim()
