5f453d5de08e20c7bcbcabe76a0921a7d513d666
imknown <imknown@qq.com>
2022-07-16 17:21:31 +0800

    Update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -88,7 +88,7 @@ object Versions {
         const val googleServices = "4.3.13"
 
         // https://firebase.google.com/docs/android/learn-more#bom
-        const val billOfMaterials = "30.2.0"
+        const val billOfMaterials = "30.3.0"
 
         // https://firebase.google.com/docs/crashlytics/get-started?platform=android
         // https://firebase.google.com/docs/crashlytics/ndk-reports
