8a274108fef3aad58a85fb7cb883d2683b88ae9b
imknown <imknown@qq.com>
2022-08-31 09:02:20 +0800

    Update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -24,7 +24,7 @@ object Versions {
     const val desugarJdkLibs = "1.2.2"
 
     // https://github.com/material-components/material-components-android/releases
-    const val material = "1.7.0-beta01"
+    const val material = "1.7.0-rc01"
 
     // region [AndroidX]
     // https://maven.google.com
