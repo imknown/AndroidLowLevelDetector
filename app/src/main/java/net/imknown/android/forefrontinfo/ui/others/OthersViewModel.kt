f65778bae1b8850c1fd20934c86186bc027ba260
imknown <imknown@qq.com>
2022-10-28 21:40:51 +0800

    Improve fingerprint detecting logic

--- app/src/main/java/net/imknown/android/forefrontinfo/ui/others/OthersViewModel.kt
+++ app/src/main/java/net/imknown/android/forefrontinfo/ui/others/OthersViewModel.kt
@@ -244,6 +244,7 @@ class OthersViewModel : BasePureListViewModel(), IAndroidVersion {
         val partitions = arrayOf(
             "bootimage",
             "odm",
+            "oem",
             "product",
             "system",
             "system_ext",
@@ -254,7 +255,7 @@ class OthersViewModel : BasePureListViewModel(), IAndroidVersion {
         partitions.forEach {
             val partitionFingerprintProperty = partitionFingerprint(it)
             val fingerprint = getStringProperty(partitionFingerprintProperty)
-            if (fingerprint != MyApplication.getMyString(android.R.string.unknownName)) {
+            if (fingerprint != MyApplication.getMyString(R.string.build_not_filled)) {
                 add(
                     tempModels,
                     MyApplication.getMyString(R.string.build_certain_fingerprint, it),
