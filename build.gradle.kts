b4bbfe240d9f6bf9d5edba937d9ad27a73b37dd2
imknown <imknown@qq.com>
2024-08-13 10:10:43 +0800

    Feat: (WIP) Shizuku
    Signed-off-by: imknown <imknown@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
     alias(libsKotlin.plugins.kotlin.android) apply false
     alias(libsKotlin.plugins.kotlinx.serialization) apply false
+    alias(libsKotlin.plugins.kotlinx.parcelize) apply false
 
     alias(libsGoogle.plugins.googleServices) apply false
     alias(libsGoogle.plugins.firebase.crashlytics) apply false
