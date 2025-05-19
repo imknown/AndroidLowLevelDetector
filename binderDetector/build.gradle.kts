9506ca65ad6680e5fad7c7b9cbda608f7440ca1b
imknown <imknown@qq.com>
2025-05-19 15:07:08 +0800

    Feat: (WIP) C to Kotlin/Native

--- binderDetector/build.gradle.kts
+++ binderDetector/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libsAndroid.plugins.android.library)
 
-    alias(libsKotlin.plugins.kotlin.android)
+    alias(libsKotlin.plugins.kotlin.multiplatform)
 }
 
 android {
@@ -20,25 +20,25 @@ android {
     defaultConfig {
         minSdk = libsBuild.versions.minSdk.get().toInt()
 
-        externalNativeBuild {
-            cmake {
-                arguments += listOf("-DANDROID_ARM_NEON=TRUE", "-DANDROID_TOOLCHAIN=clang")
-
-                cFlags += listOf("-D__STDC_FORMAT_MACROS")
-
-                cppFlags += listOf("-fexceptions", "-frtti", "-std=c++17")
-            }
-        }
+//        externalNativeBuild {
+//            cmake {
+//                arguments += listOf("-DANDROID_ARM_NEON=TRUE", "-DANDROID_TOOLCHAIN=clang")
+//
+//                cFlags += listOf("-D__STDC_FORMAT_MACROS")
+//
+//                cppFlags += listOf("-fexceptions", "-frtti", "-std=c++17")
+//            }
+//        }
     }
 
     ndkVersion = libsBuild.versions.ndk.get()
 
-    externalNativeBuild {
-        cmake {
-            path("src/main/cpp/CMakeLists.txt")
-            version = libsBuild.versions.cmake.get()
-        }
-    }
+//    externalNativeBuild {
+//        cmake {
+//            path("src/main/cpp/CMakeLists.txt")
+//            version = libsBuild.versions.cmake.get()
+//        }
+//    }
 }
 
 // region [Toolchain]
@@ -48,5 +48,27 @@ android {
 private val javaToolchain = libsBuild.versions.javaToolchain.get().toInt()
 kotlin {
     jvmToolchain(javaToolchain)
+
+    androidTarget()
+
+    val androidTargets = listOf(
+        androidNativeArm64(),
+        androidNativeArm32(),
+        androidNativeX64(),
+        androidNativeX86()
+    )
+    androidTargets.forEach {
+        it.binaries {
+            executable()
+//            sharedLib("BinderDetector")
+        }
+
+        with(it) {
+            compilations.getByName("main") {
+                val binderDetector by cinterops.creating {
+                }
+            }
+        }
+    }
 }
 // endregion [Toolchain]
