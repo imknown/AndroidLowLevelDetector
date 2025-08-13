f63846eb6d2856293fd3111575d9f230e31cd7fc
imknown <imknown@qq.com>
2025-08-13 13:18:56 +0800

    Fix: Avoid Android 5.0 ~ 7.1.2 crash: "key.length > 31"
    # See: https://android.googlesource.com/platform/frameworks/base/+/refs/tags/android-7.1.2_r1/core/java/android/os/SystemProperties.java

--- app/src/main/java/net/imknown/android/forefrontinfo/ui/common/PropertyExt.kt
+++ app/src/main/java/net/imknown/android/forefrontinfo/ui/common/PropertyExt.kt
@@ -9,8 +9,8 @@ fun getStringProperty(
 ): String {
     val notSupport = MyApplication.getMyString(R.string.result_not_supported)
     return if (
-        isAtLeastStableAndroid6()
-        || key.length < 31 // Avoid Android 5.0 crash: "key.length > 31"
+        isAtLeastStableAndroid8()
+        || key.length <= 31 // Avoid Android 5.0 ~ 7.1.2 crash: "key.length > 31"
     ) {
         if (condition) {
             PropertyManager.instance.getString(key, MyApplication.getMyString(R.string.build_not_filled))
