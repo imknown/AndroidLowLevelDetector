e131df7794cad67f4c52104f4af3f6b912500f22
imknown <imknown@qq.com>
2026-05-18 13:16:13 +0800

    Chore: Add ProjectLibProvider
    Signed-off-by: imknown <imknown@qq.com>

--- build-logic/convention/src/main/kotlin/net/imknown/android/forefrontinfo/ext/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/net/imknown/android/forefrontinfo/ext/ProjectExtensions.kt
@@ -4,7 +4,6 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.support.uppercaseFirstChar
 import kotlin.reflect.KProperty
 
 fun VersionCatalog.findVersionString(alias: String) = findVersion(alias).get().toString()
@@ -15,14 +14,15 @@ fun VersionCatalog.findPluginId(alias: String) = findPlugin(alias).get().get().p
 fun Project.androidPlugin(alias: String) = libsAndroid.findPluginId(alias)
 fun Project.googlePlugin(alias: String) = libsGoogle.findPluginId(alias)
 
-fun Project.libs(suffix: String) =
-    extensions.getByType<VersionCatalogsExtension>()
-        .named("libs${suffix.uppercaseFirstChar()}")
-val Project.libsBuild get() = libs("build")
-val Project.libsAndroid get() = libs("android")
-val Project.libsKotlin get() = libs("kotlin")
-val Project.libsGoogle get() = libs("google")
-val Project.libsThirdParty get() = libs("thirdParty")
+object ProjectLibProvider {
+    operator fun getValue(thisRef: Project, property: KProperty<*>): VersionCatalog =
+        thisRef.extensions.getByType<VersionCatalogsExtension>().named(property.name)
+}
+val Project.libsBuild: VersionCatalog by ProjectLibProvider
+val Project.libsAndroid: VersionCatalog by ProjectLibProvider
+// val Project.libsKotlin: VersionCatalog by ProjectLibProvider
+val Project.libsGoogle: VersionCatalog by ProjectLibProvider
+// val Project.libsThirdParty: VersionCatalog by ProjectLibProvider
 
 object NameProvider {
     operator fun getValue(thisRef: Any?, property: KProperty<*>) = property.name
