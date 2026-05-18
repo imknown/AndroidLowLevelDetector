c16f21fb9a69826866141deeb45268f248b98e0f
imknown <imknown@qq.com>
2026-05-18 13:33:27 +0800

    Chore: Fix lint: ProjectExtensions.kt
    Signed-off-by: imknown <imknown@qq.com>

--- build-logic/convention/src/main/kotlin/net/imknown/android/forefrontinfo/ext/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/net/imknown/android/forefrontinfo/ext/ProjectExtensions.kt
@@ -6,13 +6,13 @@ import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
 import kotlin.reflect.KProperty
 
-fun VersionCatalog.findVersionString(alias: String) = findVersion(alias).get().toString()
+fun VersionCatalog.findVersionString(alias: String): String = findVersion(alias).get().toString()
 
-fun Project.buildVersion(alias: String) = libsBuild.findVersionString(alias)
+fun Project.buildVersion(alias: String): String = libsBuild.findVersionString(alias)
 
-fun VersionCatalog.findPluginId(alias: String) = findPlugin(alias).get().get().pluginId
-fun Project.androidPlugin(alias: String) = libsAndroid.findPluginId(alias)
-fun Project.googlePlugin(alias: String) = libsGoogle.findPluginId(alias)
+fun VersionCatalog.findPluginId(alias: String): String = findPlugin(alias).get().get().pluginId
+fun Project.androidPlugin(alias: String): String = libsAndroid.findPluginId(alias)
+fun Project.googlePlugin(alias: String): String = libsGoogle.findPluginId(alias)
 
 object ProjectLibProvider {
     operator fun getValue(thisRef: Project, property: KProperty<*>): VersionCatalog =
@@ -25,8 +25,8 @@ val Project.libsGoogle: VersionCatalog by ProjectLibProvider
 // val Project.libsThirdParty: VersionCatalog by ProjectLibProvider
 
 object NameProvider {
-    operator fun getValue(thisRef: Any?, property: KProperty<*>) = property.name
+    operator fun getValue(thisRef: Nothing?, property: KProperty<*>): String = property.name
 }
-val implementation by NameProvider
-val testImplementation by NameProvider
-val androidTestImplementation by NameProvider
+val implementation: String by NameProvider
+val testImplementation: String by NameProvider
+val androidTestImplementation: String by NameProvider
